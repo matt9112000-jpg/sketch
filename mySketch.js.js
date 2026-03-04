616ab73c1ed9e20e21731bd85b6482cc9bf2556f
matt9112000-jpg <matt9112000@gmail.com>
2026-03-04 17:11:13 +0800

    5:11

--- mySketch.js.js
+++ mySketch.js.js
@@ -779,6 +779,7 @@ function makeResultVoxelGroup(snapshot, panelRoot, cubeTemplate){
   }
   group.rotation.set(Math.PI / 2, 0, 0);
   group.position.y += z + panelBox.max.y + cell * 0.12;
+  group.position.add(new THREE.Vector3(-5, -5, -5));
   return group.children.length ? group : null;
 }
 
